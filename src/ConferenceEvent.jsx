f224b7136ad06ff0483bcccd1ee618e62033d37d
John Wilding <wilding30@gmail.com>
2024-09-04 16:48:03 -0400

    completed practice up to stage 19

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -7,6 +7,7 @@ const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
+    const avItems = useSelector((state) => state.av);
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
 
@@ -157,7 +158,20 @@ const ConferenceEvent = () => {
 
                                 </div>
                                 <div className="addons_selection">
-
+                                    {avItems.map((item, index) =>(
+                                        <div className="av_data venue_main" key={index}>
+                                            <div className="img">
+                                                <img src={item.img} alt={item.name} />
+                                            </div>
+                                            <div className="text"> {item.name} </div>
+                                            <div> ${item.cost} </div>
+                                            <div className="addon_btn">
+                                                <button className="btn-warning" conClick={()_ => handleDecrementAvQuantity(item)}> &ndash; </button>
+                                                <span className="quantity-value">{item.quantity}</span>
+                                                <button className="btn-success" onClick={() => handleIncrementAvQuantity(item)}> &#43; </button>
+                                            </div>
+                                        </div>
+                                    ))}
                                 </div>
                                 <div className="total_cost">Total Cost:</div>
 
